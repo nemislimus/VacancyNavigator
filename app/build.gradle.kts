072910c923d503b1281f64b08a413d3d5c0014cc
nemislimus <nemislimus@yandex.ru>
2024-11-09 16:09:13 +0300

    E0-7: fix for detekt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
-    id ("kotlin-kapt")
+    id("kotlin-kapt")
 }
 
 android {
