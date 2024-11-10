e801bd15f5a004f29fd8015e2f97a93fbda9646b
Andrey <amdoit@yandex.ru>
2024-11-11 01:39:25 +0700

    #29 перенес каждый агрумент на отдельную строчку - того требует detekt

--- app/src/main/java/ru/practicum/android/diploma/util/DpToPx.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/DpToPx.kt
@@ -6,7 +6,9 @@ import android.util.TypedValue
 interface DpToPx {
     fun dpToPx(dp: Float, context: Context): Int {
         return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics
         ).toInt()
     }
 }
