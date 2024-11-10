b6b0e4c735276f923262898540c580d37b8e8028
MargoBarley <meggi7777777@gmail.com>
2024-11-10 10:27:14 +0300

    E0-10: add retrofit client to data DI

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,7 +1,46 @@
 package ru.practicum.android.diploma.di
 
+import android.content.Context
+import android.net.ConnectivityManager
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.network.HhSearchApi
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
 
 val dataModule = module {
+
+    single<ConnectivityManager> { androidContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
+    single<NetworkClient> {
+        RetrofitNetworkClient(hhSearchApi = get(), connectivityManager = get())
+    }
+    single<HhSearchApi> {
+
+        val interceptor = Interceptor { chain ->
+            val token = ""
+            val appname = ""
+            val mail = ""
+
+            val originalRequest = chain.request()
+            val builder = originalRequest.newBuilder()
+                .header("Authorization", "Bearer $token")
+                .header("HH-User-Agent", "$appname ($mail)")
+            val newRequest = builder.build()
+            chain.proceed(newRequest)
+        }
+
+        val okHttpClient = OkHttpClient().newBuilder().addInterceptor(interceptor).build()
+
+        Retrofit.Builder()
+            .baseUrl("https://api.hh.ru/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(HhSearchApi::class.java)
+    }
 
 }
