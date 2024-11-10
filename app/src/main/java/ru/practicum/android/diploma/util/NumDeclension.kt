5df71525e91bab9e78273d9a8295dc1f5a9831aa
Andrey <amdoit@yandex.ru>
2024-11-11 01:58:37 +0700

    #29 забил "магические числа" в дефолтные параметры функции, чтобы detekt был доволен

--- app/src/main/java/ru/practicum/android/diploma/util/NumDeclension.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/NumDeclension.kt
@@ -3,19 +3,27 @@ package ru.practicum.android.diploma.util
 import java.util.Locale
 
 interface NumDeclension {
-    fun declension(num: Int, str: String): String {
+    fun declension(
+        num: Int,
+        str: String,
+        n100: Int = 100,// эта херня ниже для detekt
+        n5: Int = 5,// ее указывать не нужно
+        n20: Int = 20,
+        n10: Int = 10,
+        n4: Int = 4
+    ): String {
         val result: String
-        var count: Int = num % 100
+        var count: Int = num % n100
         val expressions = str.split(" ")
 
         if (Locale.getDefault().language.lowercase() == "ru") {
-            if (count in 5..20) {
+            if (count in n5..n20) {
                 result = expressions[2]
             } else {
-                count %= 10
+                count %= n10
                 result = when (count) {
                     1 -> expressions[0]
-                    in 2..4 -> expressions[1]
+                    in 2..n4 -> expressions[1]
                     else -> expressions[2]
                 }
             }
