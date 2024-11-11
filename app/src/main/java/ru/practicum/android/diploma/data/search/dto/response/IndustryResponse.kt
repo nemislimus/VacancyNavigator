f4eec33dca512f05ac754bea02b8f00d3f896424
Yarogi <yachmenyov.igor@yandex.ru>
2024-11-11 17:40:32 +0300

    E0-45: Добавлены классы Response

---
+++ app/src/main/java/ru/practicum/android/diploma/data/search/dto/response/IndustryResponse.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.data.search.dto.response
+
+import ru.practicum.android.diploma.data.network.Response
+import ru.practicum.android.diploma.data.search.dto.model.IndustryDto
+
+class IndustryResponse(val result: List<IndustryDto>) : Response()
