1ae24c66d888c2d0e1794df171662451b6791ca2
Yarogi <yachmenyov.igor@yandex.ru>
2024-11-11 12:48:16 +0300

    E0-10: add getAreasByCountry

--- app/src/main/java/ru/practicum/android/diploma/data/network/HhSearchApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HhSearchApi.kt
@@ -5,12 +5,16 @@ import retrofit2.http.Path
 import retrofit2.http.QueryMap
 import ru.practicum.android.diploma.data.dto.AreaDto
 import ru.practicum.android.diploma.data.dto.CountryDto
+import ru.practicum.android.diploma.data.dto.VacancyDetailedDto
 import ru.practicum.android.diploma.data.dto.VacancyDto
 
 interface HhSearchApi {
 
     @GET("vacancies")
     suspend fun searchVacancies(@QueryMap options: Map<String, String>): List<VacancyDto>
+
+    @GET("vacancies/{id}")
+    suspend fun getVacancyDetails(@Path("id") vacancyId: String): VacancyDetailedDto
 
     @GET("areas/countries")
     suspend fun getCountries(): List<CountryDto>
