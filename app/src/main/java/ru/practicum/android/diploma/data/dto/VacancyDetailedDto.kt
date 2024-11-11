bbcfd9e44fa3141e71b8d273d8b22823f75bb20c
Yarogi <yachmenyov.igor@yandex.ru>
2024-11-11 13:01:23 +0300

    E0-10: add detail in vacanceDetailedDto

--- app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDetailedDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDetailedDto.kt
@@ -1,6 +1,14 @@
 package ru.practicum.android.diploma.data.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class VacancyDetailedDto(
     val id: String,
     val name: String,
+    val experience: ExperienceDto? = null,
+    val schedule: ScheduleDto? = null,
+    val employment: EmploymentDto? = null,
+    val description: String? = null,
+    @SerializedName("branded_description") val brandedDescription: String? = null,
+    @SerializedName("key_skills") val keySkills: List<SkillDto>? = null,
 )
