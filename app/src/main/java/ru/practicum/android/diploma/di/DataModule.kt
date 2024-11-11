0539510c3b6c68d83fe7a2c0532233b93263a8d0
Andrey <amdoit@yandex.ru>
2024-11-11 19:09:06 +0700

    #42 сделана инициализация базы данных

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,7 +1,13 @@
 package ru.practicum.android.diploma.di
 
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.db.XxxDataBase
 
 val dataModule = module {
 
+    single<XxxDataBase> {
+        Room.databaseBuilder(androidContext(), XxxDataBase::class.java, "xxx-team.db").build()
+    }
 }
