56331a990595116805970bf4614ea6f4be3e9c8a
MargoBarley <meggi7777777@gmail.com>
2024-11-11 21:44:06 +0300

    E0-45: Unexpected spacing before "("

--- app/src/main/java/ru/practicum/android/diploma/data/search/dto/response/VacancyDetailedResponse.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/search/dto/response/VacancyDetailedResponse.kt
@@ -3,4 +3,4 @@ package ru.practicum.android.diploma.data.search.dto.response
 import ru.practicum.android.diploma.data.network.Response
 import ru.practicum.android.diploma.data.search.dto.model.VacancyDetailedDto
 
-class VacancyDetailedResponse (val result: VacancyDetailedDto) : Response()
+class VacancyDetailedResponse(val result: VacancyDetailedDto) : Response()
--- app/src/main/java/ru/practicum/android/diploma/data/search/dto/response/VacancyResponse.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/search/dto/response/VacancyResponse.kt
@@ -3,4 +3,4 @@ package ru.practicum.android.diploma.data.search.dto.response
 import ru.practicum.android.diploma.data.network.Response
 import ru.practicum.android.diploma.data.search.dto.model.VacancyDto
 
-class VacancyResponse (val result: List<VacancyDto>) : Response()
+class VacancyResponse(val result: List<VacancyDto>) : Response()
