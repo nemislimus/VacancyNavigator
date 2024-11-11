4a7b4b0c44fa29d8d91bd822ffe09f61149f11da
Yarogi <yachmenyov.igor@yandex.ru>
2024-11-11 17:23:40 +0300

    E0-45: Добавлены классы запроса данных

---
+++ app/src/main/java/ru/practicum/android/diploma/data/search/dto/VacancyRequest.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.data.search.dto
+
+import ru.practicum.android.diploma.domain.search.model.SearchVacancyOptions
+
+class VacancyRequest(val options: SearchVacancyOptions)
