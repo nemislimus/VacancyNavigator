3f04083b95bfa8820812597b5d0bcc9ae4efa749
Yarogi <yachmenyov.igor@yandex.ru>
2024-11-11 14:11:00 +0300

    E0-10: оставлены только необходмые поля

--- app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDetailedDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDetailedDto.kt
@@ -5,10 +5,10 @@ import com.google.gson.annotations.SerializedName
 data class VacancyDetailedDto(
     val id: String,
     val name: String,
+    val salary: SalaryDto? = null,
     val experience: ExperienceDto? = null,
     val schedule: ScheduleDto? = null,
     val employment: EmploymentDto? = null,
     val description: String? = null,
-    @SerializedName("branded_description") val brandedDescription: String? = null,
     @SerializedName("key_skills") val keySkills: List<SkillDto>? = null,
 )
