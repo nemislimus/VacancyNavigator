548a338b1d54a56ea0aa40b6362076c20b12acee
nemislimus <nemislimus@yandex.ru>
2024-11-12 18:26:23 +0300

    #54 Адаптер и холдеры. Фиксы по вёрстке.

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/model/Vacancy.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.domain.search.model
+
+
+data class Vacancy(
+    val id: String,
+    val name: String,
+    val salary: String,
+    val employer: String,
+)
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/GapViewHolder.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.ui.search
+
+import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.databinding.VacancyListGapBinding
+
+class GapViewHolder(
+    private val binding: VacancyListGapBinding
+): RecyclerView.ViewHolder(binding.root)
