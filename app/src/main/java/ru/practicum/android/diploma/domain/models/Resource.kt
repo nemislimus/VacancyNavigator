d9a620edfdf3e56682b07c67cbb3f82686198717
Yarogi <yachmenyov.igor@yandex.ru>
2024-11-12 15:49:34 +0300

    E0-52: Добавлен Resource - обертка для обработки результатов от Networkclient

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Resource.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.domain.models
+
+sealed interface Resource<T> {
+    data class Success<T>(val data: T) : Resource<T>
+    data class Error<T>(val message: String) : Resource<T>
+}
