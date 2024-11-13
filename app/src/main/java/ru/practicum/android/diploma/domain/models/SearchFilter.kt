18cfa57fe0a62bb62392aea115a02d9ecc772d52
MargoBarley <meggi7777777@gmail.com>
2024-11-14 02:45:36 +0300

    E0-77: SearchFilter изменил на data-class + добавил описание

--- app/src/main/java/ru/practicum/android/diploma/domain/models/SearchFilter.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/SearchFilter.kt
@@ -1,11 +1,21 @@
 package ru.practicum.android.diploma.domain.models
 
-class SearchFilter(
-    var country: Area? = null, // страна
-    var region: Area? = null, // регион
-    var city: Area? = null, // город
-    var industry: Industry? = null, // Индустрия
-    var salary: Int? = null, // ожидаемая зарпала
-    var onlyWithSalary: Boolean? = null, // показывать только с зарплатой
-    var geolocation: Geolocation? = null // показать вакансии рядом со мной (по геолокации)
+/**
+ * Фильтр для поиска вакансий
+ * @param country - страна
+ * @param region - регион
+ * @param city - город
+ * @param industry - отрасль
+ * @param salary - ожидаемая зарпала
+ * @param onlyWithSalary -  показывать только с зарплатой
+ * @param geolocation - показать вакансии рядом со мной (по геолокации)
+ * */
+data class SearchFilter(
+    val country: Area? = null,
+    val region: Area? = null,
+    val city: Area? = null,
+    val industry: Industry? = null,
+    val salary: Int? = null,
+    val onlyWithSalary: Boolean? = null,
+    val geolocation: Geolocation? = null
 )
