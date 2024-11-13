0e13692e8badc4785f3d7b31d4f4d34d9f8f8e3e
MargoBarley <meggi7777777@gmail.com>
2024-11-14 02:48:22 +0300

    E0-77: SearchVacancyOptions изменил на data-class + добавил описание

--- app/src/main/java/ru/practicum/android/diploma/domain/search/model/SearchVacancyOptions.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/model/SearchVacancyOptions.kt
@@ -2,9 +2,13 @@ package ru.practicum.android.diploma.domain.search.model
 
 import ru.practicum.android.diploma.domain.models.SearchFilter
 
-class SearchVacancyOptions(
-    var text: String,
-    var page: Int = 0,
-) {
-    val filter: SearchFilter = SearchFilter()
-}
+/** Параметры поиска
+ * @param text - Наименование вакансии или ещё часть
+ * @param page - Страница списка
+ * @param filter - Фильтр вакансий
+ * */
+data class SearchVacancyOptions(
+    val text: String,
+    val page: Int = 0,
+    val filter: SearchFilter? = null
+)
