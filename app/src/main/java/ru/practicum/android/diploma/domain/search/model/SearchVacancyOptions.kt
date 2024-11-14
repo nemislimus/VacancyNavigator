26d9f0249077680f37006747afed81941d7a9bb0
MargoBarley <meggi7777777@gmail.com>
2024-11-14 03:07:12 +0300

    E0-83: SearchVacancyOptions - добавил описание

--- app/src/main/java/ru/practicum/android/diploma/domain/search/model/SearchVacancyOptions.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/model/SearchVacancyOptions.kt
@@ -5,7 +5,8 @@ import ru.practicum.android.diploma.domain.models.SearchFilter
 /** Параметры поиска
  * @param text - Наименование вакансии или ещё часть
  * @param page - Страница списка
- * @param filter - Фильтр вакансий
+ * @param filter - Фильтр вакансий [SearchFilter]
+ * @author Ячменев И.
  * */
 data class SearchVacancyOptions(
     val text: String,
