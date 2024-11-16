783f76006db8a8d42eae88935553dcb19267def3
MargoBarley <meggi7777777@gmail.com>
2024-11-16 17:06:54 +0300

    E1-104: добавлен шаблон для интератора

--- app/src/main/java/ru/practicum/android/diploma/domain/search/api/SearchInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/api/SearchInteractor.kt
@@ -4,6 +4,6 @@ import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.models.VacancyShort
 
 interface SearchInteractor {
-    suspend fun searchVacancy(text: String, page: Int): Flow<List<VacancyShort>>
+    suspend fun searchVacancy(text: String, page: Int): Flow<Result<List<VacancyShort>>>
     suspend fun hasActiveFilter(): Flow<Boolean>
 }
--- app/src/main/java/ru/practicum/android/diploma/domain/search/api/SearchRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/api/SearchRepository.kt
@@ -1,9 +1,10 @@
 package ru.practicum.android.diploma.domain.search.api
 
 import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.Resource
 import ru.practicum.android.diploma.domain.models.VacancyShort
 import ru.practicum.android.diploma.domain.search.model.SearchVacancyOptions
 
 interface SearchRepository {
-    suspend fun searchVacancy(searchOptions: SearchVacancyOptions): Flow<List<VacancyShort>>
+    suspend fun searchVacancy(searchOptions: SearchVacancyOptions): Flow<Resource<List<VacancyShort>>>
 }
