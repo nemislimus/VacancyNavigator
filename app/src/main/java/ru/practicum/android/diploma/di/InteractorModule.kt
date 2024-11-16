2b6c5e5df8064539d4e356e054fecca71aa2bb79
MargoBarley <meggi7777777@gmail.com>
2024-11-16 17:10:23 +0300

    E1-104: интерактор включен в di

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,7 +1,9 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.search.api.SearchInteractor
+import ru.practicum.android.diploma.domain.search.impl.SearchInteractorImpl
 
 val interactorModule = module {
-
+    single<SearchInteractor> { SearchInteractorImpl(searchRepository = get(), filterRepository = get()) }
 }
