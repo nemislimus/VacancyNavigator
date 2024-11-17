cebbf269e58b0b1819db214a39e056f3060cbe01
Andrey <amdoit@yandex.ru>
2024-11-17 11:38:11 +0700

    #105 явно добавлен тип CoroutineScope иначе детект считает, что есть неиспользуемые импорты

---
+++ app/src/main/java/ru/practicum/android/diploma/di/UiModule.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.di
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import org.koin.dsl.module
+
+val uiModule = module {
+
+}
--- app/src/main/java/ru/practicum/android/diploma/ui/root/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/App.kt
@@ -3,8 +3,6 @@ package ru.practicum.android.diploma.ui.root
 import android.app.Application
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
@@ -12,11 +10,11 @@ import org.koin.core.context.GlobalContext.startKoin
 import ru.practicum.android.diploma.di.dataModule
 import ru.practicum.android.diploma.di.interactorModule
 import ru.practicum.android.diploma.di.repositoryModule
+import ru.practicum.android.diploma.di.uiModule
 import ru.practicum.android.diploma.di.viewModelModule
 import ru.practicum.android.diploma.domain.impl.UpdateDbOnAppStartUseCase
 
 class App : Application() {
-    private val scope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate() {
         super.onCreate()
@@ -25,18 +23,15 @@ class App : Application() {
             // Метод специального класса, переданного как this, для добавления контекста в граф
             androidContext(this@App)
             // Передаём все модули, чтобы их содержимое было передано в граф
-            modules(repositoryModule, interactorModule, viewModelModule, dataModule)
+            modules(repositoryModule, interactorModule, viewModelModule, dataModule, uiModule)
         }
 
         val dataUpdater: UpdateDbOnAppStartUseCase by inject()
 
+        val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+
         scope.launch {
             dataUpdater()
         }
-    }
-
-    override fun onTerminate() {
-        super.onTerminate()
-        scope.cancel()
     }
 }
