f6a43d178b5ab18e8d5e61652fb310bbc39b6d71
Andrey <amdoit@yandex.ru>
2024-11-17 18:12:32 +0700

    #88 убрал неиспользуемые импорты

--- app/src/main/java/ru/practicum/android/diploma/domain/repository/GetSearchFilterRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/repository/GetSearchFilterRepository.kt
@@ -1,9 +1,6 @@
 package ru.practicum.android.diploma.domain.repository
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.domain.models.Area
-import ru.practicum.android.diploma.domain.models.Geolocation
-import ru.practicum.android.diploma.domain.models.Industry
 import ru.practicum.android.diploma.domain.models.SearchFilter
 
 interface GetSearchFilterRepository {
