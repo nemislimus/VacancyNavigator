d0c1d317fb02b9b389ac29705b9b2efb2268e850
MargoBarley <meggi7777777@gmail.com>
2024-11-17 16:44:18 +0300

    E1-87: реализация поиска в интерактор + исправление ошибке возврата

--- app/src/main/java/ru/practicum/android/diploma/domain/search/api/SearchInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/api/SearchInteractor.kt
@@ -1,9 +1,10 @@
 package ru.practicum.android.diploma.domain.search.api
 
 import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.Resource
 import ru.practicum.android.diploma.domain.models.VacancyShort
 
 interface SearchInteractor {
-    suspend fun searchVacancy(text: String, page: Int): Flow<Result<List<VacancyShort>>>
+    suspend fun searchVacancy(text: String, page: Int): Flow<Resource<List<VacancyShort>>>
     suspend fun hasActiveFilter(): Flow<Boolean>
 }
