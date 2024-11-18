69e9cffe18199027714d40260bd054adb694844b
underbreathing <artem-sheverdyaev@mail.ru>
2024-11-19 00:41:00 +0300

    Реализовал функционал поиска в SearchFragment

--- app/src/main/java/ru/practicum/android/diploma/ui/search/fragments/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/fragments/SearchFragment.kt
@@ -9,10 +9,13 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.domain.models.VacancyShort
-import ru.practicum.android.diploma.ui.search.VacancyListAdapter
+import ru.practicum.android.diploma.ui.search.rv.VacancyListAdapter
+import ru.practicum.android.diploma.ui.search.viewmodels.SearchState
+import ru.practicum.android.diploma.ui.search.viewmodels.SearchViewModel
 import ru.practicum.android.diploma.ui.utils.MenuBindingFragment
 import ru.practicum.android.diploma.ui.vacancy.VacancyFragment
 import ru.practicum.android.diploma.util.EMPTY_STRING
@@ -20,6 +23,8 @@ import ru.practicum.android.diploma.util.EMPTY_STRING
 class SearchFragment : MenuBindingFragment<FragmentSearchBinding>() {
 
     private val listAdapter = VacancyListAdapter { clickOnVacancy(it) }
+
+    private val viewModel: SearchViewModel by viewModel()
 
     override fun createBinding(
         inflater: LayoutInflater,
@@ -44,15 +49,76 @@ class SearchFragment : MenuBindingFragment<FragmentSearchBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.rvVacancyList.adapter = listAdapter
-
         binding.llSearchFieldContainer.ivClearIcon.setOnClickListener {
             clearQuery()
         }
 
         binding.llSearchFieldContainer.etSearchQueryText.addTextChangedListener { s ->
+            if (s.isNullOrBlank()) {
+                clearScreen()
+            }
+            viewModel.searchDebounce(s.toString())
             setSearchIcon(s.isNullOrBlank())
             showIntro(s.isNullOrBlank())
         }
+
+        viewModel.searchState.observe(viewLifecycleOwner) { searchResult ->
+            when (searchResult) {
+                SearchState.ConnectionError -> showConnectionError()
+                is SearchState.Content -> showContent(searchResult.pageData)
+                SearchState.IsLoading -> showLoading()
+                SearchState.NotFoundError -> showNotFoundError()
+            }
+        }
+
+    }
+
+    private fun showErrorBase() {
+        with(binding) {
+            pbSearchProgress.isVisible = false
+            rvVacancyList.isVisible = false
+            clPlaceholder.root.isVisible = true
+        }
+    }
+
+    private fun showNotFoundError() {
+        showErrorBase()
+        with(binding) {
+            clPlaceholder.tvPlaceholderText.text = getString(R.string.not_found_vacancies)
+            clPlaceholder.ivPlaceholderPicture.setImageResource(R.drawable.placeholder_not_found_picture)
+        }
+    }
+
+    private fun showConnectionError() {
+        showErrorBase()
+        with(binding) {
+            clPlaceholder.tvPlaceholderText.text = getString(R.string.no_internet)
+            clPlaceholder.ivPlaceholderPicture.setImageResource(R.drawable.placeholder_no_internet_picture)
+        }
+    }
+
+    private fun clearScreen() {
+        listAdapter.submitList(emptyList())
+        clearPlaceholders()
+    }
+
+    private fun clearPlaceholders() {
+        with(binding) {
+            clPlaceholder.root.isVisible = false
+            pbSearchProgress.isVisible = false
+        }
+    }
+
+    private fun showLoading() {
+        with(binding) {
+            pbSearchProgress.isVisible = true
+        }
+    }
+
+    private fun showContent(searchData: List<VacancyShort>) {
+        binding.rvVacancyList.isVisible = true
+        clearPlaceholders()
+        listAdapter.submitList(searchData)
     }
 
     private fun setSearchIcon(queryIsEmpty: Boolean) {
@@ -67,7 +133,11 @@ class SearchFragment : MenuBindingFragment<FragmentSearchBinding>() {
     }
 
     private fun clearQuery() {
-        binding.llSearchFieldContainer.etSearchQueryText.setText(EMPTY_STRING)
+        with(binding) {
+            llSearchFieldContainer.etSearchQueryText.setText(EMPTY_STRING)
+            rvVacancyList.isVisible = false
+            pbSearchProgress.isVisible = false
+        }
     }
 
     private fun goToFilter() {
