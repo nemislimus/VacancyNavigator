6c012ba054aa1d2bb10a025372933f700954fe36
underbreathing <artem-sheverdyaev@mail.ru>
2024-11-19 00:39:57 +0300

    создал viewModel для SearchFragment

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -2,10 +2,15 @@ package ru.practicum.android.diploma.di
 
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.ui.search.viewmodels.SearchViewModel
 import ru.practicum.android.diploma.ui.vacancy.viewmodels.VacancyViewModel
 import ru.practicum.android.diploma.viewmodels.RootActivityViewModel
 
 val viewModelModule = module {
+    viewModel {
+        SearchViewModel(get())
+    }
+
     viewModel {
         RootActivityViewModel()
     }
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/viewmodels/SearchState.kt
@@ -0,0 +1,15 @@
+package ru.practicum.android.diploma.ui.search.viewmodels
+
+import ru.practicum.android.diploma.domain.models.VacancyShort
+
+sealed interface SearchState {
+
+    data object IsLoading : SearchState
+
+    data object ConnectionError : SearchState
+
+    data object NotFoundError : SearchState
+
+    data class Content(val pageData: List<VacancyShort>) : SearchState
+
+}
