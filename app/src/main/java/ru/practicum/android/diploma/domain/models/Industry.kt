571d34fbac4ef47ce9027abec3d4a649e3c2b82e
nemislimus <nemislimus@yandex.ru>
2024-11-22 18:44:34 +0300

    #171 detekt fix 3 lol

--- app/src/main/java/ru/practicum/android/diploma/domain/models/Industry.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Industry.kt
@@ -4,5 +4,5 @@ data class Industry(
     val id: String,
     val name: String,
     val parentId: String?,
-    var isSelected: Boolean = false // Это поле нужно для работы в адаптере выбора отраслей
+    val isSelected: Boolean = false // Это поле нужно для работы в адаптере выбора отраслей
 )
