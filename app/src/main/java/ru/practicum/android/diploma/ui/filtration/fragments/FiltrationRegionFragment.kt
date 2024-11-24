4b07a8b54a52ba882bf85533980b9f91ba515fa0
underbreathing <artem-sheverdyaev@mail.ru>
2024-11-24 18:46:37 +0300

    Создал все основные классы для реализации экрана фильтрации по регионам

--- app/src/main/java/ru/practicum/android/diploma/ui/filtration/fragments/FiltrationRegionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/fragments/FiltrationRegionFragment.kt
@@ -1,16 +1,138 @@
 package ru.practicum.android.diploma.ui.filtration.fragments
 
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import ru.practicum.android.diploma.databinding.FragmentFiltrationSelectBinding
+import android.view.inputmethod.InputMethodManager
+import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentFiltrationRegionBinding
+import ru.practicum.android.diploma.domain.models.Area
+import ru.practicum.android.diploma.ui.filtration.adapters.RegionAdapter
+import ru.practicum.android.diploma.ui.filtration.viewmodels.FiltrationRegionData
+import ru.practicum.android.diploma.ui.filtration.viewmodels.FiltrationRegionViewModel
 import ru.practicum.android.diploma.ui.utils.BindingFragment
+import ru.practicum.android.diploma.util.EMPTY_STRING
 
-class FiltrationRegionFragment : BindingFragment<FragmentFiltrationSelectBinding>() {
 
-    override fun createBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): FragmentFiltrationSelectBinding {
-        return FragmentFiltrationSelectBinding.inflate(inflater, container, false)
+class FiltrationRegionFragment : BindingFragment<FragmentFiltrationRegionBinding>() {
+
+    private val viewModel: FiltrationRegionViewModel by viewModel {
+        val countryId: String? = arguments?.getString(COUNTRY_ID_KEY)
+        parametersOf(countryId)
     }
+
+    private val listAdapter = RegionAdapter {
+        viewModel.saveRegion(it)
+    }
+
+    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentFiltrationRegionBinding {
+        return FragmentFiltrationRegionBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.llSearchRegionField.etSearchQueryText.hint = requireContext().getText(R.string.enter_region_query)
+
+
+        with(binding) {
+            tbRegionToolBar.setOnClickListener { goBack() }
+            rvIndustryist.adapter = listAdapter
+            llSearchRegionField.ivClearIcon.setOnClickListener {
+                clearQuery()
+            }
+
+            llSearchRegionField.etSearchQueryText.addTextChangedListener { s ->
+                setSearchIcon(s.isNullOrBlank())
+                viewModel.getRegions(s.toString())
+            }
+
+        }
+
+        viewModel.liveData.observe(viewLifecycleOwner) {
+            when (it) {
+                FiltrationRegionData.GoBack -> goBack()
+                FiltrationRegionData.NotFound -> showNotFound()
+                FiltrationRegionData.NotSuchRegion -> shoNotSuchRegion()
+                is FiltrationRegionData.Regions -> showRegions(it.regions)
+            }
+        }
+
+    }
+
+    private fun shoNotSuchRegion() {
+        TODO()
+    }
+
+    private fun showRegions(regions: List<Area>) {
+        setPlaceholdersVisibility(false)
+        binding.rvIndustryist.isVisible = true
+        listAdapter.submitList(regions)
+    }
+
+    private fun setPlaceholdersVisibility(isVisible: Boolean) {
+        binding.clPlaceholder.root.isVisible = isVisible
+    }
+
+    private fun showNotFound() {
+        listAdapter.submitList(emptyList())
+        setPlaceholdersVisibility(true)
+        with(binding) {
+            rvIndustryist.isVisible = false
+            clPlaceholder.ivPlaceholderPicture.setImageResource(R.drawable.placeholder_filter_region_list_not_found)
+            clPlaceholder.tvPlaceholderText.text = requireContext().getText(R.string.not_found_regions)
+        }
+    }
+
+    private fun setSearchIcon(queryIsEmpty: Boolean) {
+        with(binding.llSearchRegionField) {
+            ivSearchIcon.isVisible = queryIsEmpty
+            ivClearIcon.isVisible = !queryIsEmpty
+        }
+    }
+
+    private fun clearQuery() {
+        with(binding) {
+            llSearchRegionField.etSearchQueryText.setText(EMPTY_STRING)
+        }
+        closeKeyboard()
+    }
+
+    private fun closeKeyboard() {
+        activity?.let {
+            it.currentFocus?.let { view ->
+                val manager = requireActivity().baseContext.getSystemService(
+                    INPUT_METHOD_SERVICE
+                ) as InputMethodManager
+                manager.hideSoftInputFromWindow(
+                    view.windowToken,
+                    0
+                )
+            }
+        }
+    }
+
+    private fun goBack() {
+        findNavController().navigateUp()
+        //TODO("Здесь передать результат предыдущему фрагменту")
+    }
+
+    companion object {
+
+        const val COUNTRY_ID_KEY = "country id key"
+
+        fun setParams(countryId: String): Bundle {
+            return bundleOf(COUNTRY_ID_KEY to countryId)
+        }
+    }
+
 }
