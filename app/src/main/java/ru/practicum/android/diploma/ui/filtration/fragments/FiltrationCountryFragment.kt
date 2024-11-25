4cc7696302fd78fabf00416e864cb02fc1e12199
nemislimus <nemislimus@yandex.ru>
2024-11-26 00:42:39 +0300

    #203 первая настройка экрана выбор страны

--- app/src/main/java/ru/practicum/android/diploma/ui/filtration/fragments/FiltrationCountryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/fragments/FiltrationCountryFragment.kt
@@ -4,31 +4,60 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.bundle.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
-import ru.practicum.android.diploma.databinding.FragmentFiltrationSelectBinding
+import com.google.gson.Gson
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.databinding.FragmentFiltrationCountriesBinding
+import ru.practicum.android.diploma.domain.models.Area
+import ru.practicum.android.diploma.ui.filtration.adapters.RegionAdapter
+import ru.practicum.android.diploma.ui.filtration.viewmodels.FiltrationCountriesViewModel
+import ru.practicum.android.diploma.ui.filtration.viewmodels.FiltrationCountryData
 import ru.practicum.android.diploma.ui.utils.BindingFragment
 
-class FiltrationCountryFragment : BindingFragment<FragmentFiltrationSelectBinding>() {
+class FiltrationCountryFragment : BindingFragment<FragmentFiltrationCountriesBinding>() {
+
+    private val viewModel: FiltrationCountriesViewModel by viewModel()
+
+    private val listAdapter = RegionAdapter { countrySelected(it) }
 
     override fun createBinding(
         inflater: LayoutInflater,
         container: ViewGroup?,
-    ): FragmentFiltrationSelectBinding {
-        return FragmentFiltrationSelectBinding.inflate(inflater, container, false)
+    ): FragmentFiltrationCountriesBinding {
+        return FragmentFiltrationCountriesBinding.inflate(inflater, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.rvCountryList.adapter = listAdapter
+
         binding.tbCountryWorkToolBar.setOnClickListener {
             findNavController().navigateUp()
         }
+
+        viewModel.liveData.observe(viewLifecycleOwner) { countries ->
+            manageCountriesData(countries)
+        }
     }
 
-    // Раскоментировать при работе. Закомментриовал из-за detekt
-//    private fun countrySelected(area: Area) {
-//        setFragmentResult(RESULT_COUNTRY_KEY, bundleOf(RESULT_COUNTRY_KEY to Gson().toJson(area)))
-//    }
+    private fun manageCountriesData(data: FiltrationCountryData) {
+        when (data) {
+            is FiltrationCountryData.Countries -> setCountries(data.countries)
+        }
+    }
+
+    private fun setCountries(countries: List<Area>) {
+        listAdapter.areas.addAll(countries)
+        listAdapter.notifyDataSetChanged()
+    }
+
+    private fun countrySelected(area: Area) {
+        setFragmentResult(RESULT_COUNTRY_KEY, bundleOf(RESULT_COUNTRY_KEY to Gson().toJson(area)))
+        findNavController().navigateUp()
+    }
 
     companion object {
         const val RESULT_COUNTRY_KEY = "selected_country"
