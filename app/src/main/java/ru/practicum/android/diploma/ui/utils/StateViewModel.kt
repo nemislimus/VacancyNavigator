1e94ebe1a2c60aacde17a238e32822e764073c0b
Yarogi <143076903+Yarogi@users.noreply.github.com>
2024-11-25 16:54:49 +0300

    E5-202: Добавил StateViewModel для быстрого создания ViewModel

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/utils/StateViewModel.kt
@@ -0,0 +1,16 @@
+package ru.practicum.android.diploma.ui.utils
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+abstract class StateViewModel<T> : ViewModel() {
+
+    private val stateLiveData = MutableLiveData<T>()
+    fun stateObserver(): LiveData<T> = stateLiveData
+
+    protected fun renderState(state: T) {
+        stateLiveData.postValue(state)
+    }
+
+}
