f51a54a58380a434acb1a21ccbfc74568997bae4
Yarogi <143076903+Yarogi@users.noreply.github.com>
2024-11-25 17:01:35 +0300

    E5-202: Заготовка на статусы и VM

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/viewmodels/FiltrationPlaceOfWorkViewModel.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.ui.filtration.viewmodels
+
+import ru.practicum.android.diploma.ui.utils.StateViewModel
+
+class FiltrationPlaceOfWorkViewModel : StateViewModel<FiltrationPlaceOfWorkState>() {
+
+
+
+}
