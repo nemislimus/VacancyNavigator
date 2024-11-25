0724a4c2c34e2e1693f76cdd3f9f01c9e954a9bc
Yarogi <143076903+Yarogi@users.noreply.github.com>
2024-11-25 11:57:44 +0300

    E5-195: изменен RegionAdapter на RecyclerView.Adapter

--- app/src/main/java/ru/practicum/android/diploma/ui/filtration/adapters/RegionAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/adapters/RegionAdapter.kt
@@ -2,15 +2,16 @@ package ru.practicum.android.diploma.ui.filtration.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.databinding.RvItemRegionBinding
 import ru.practicum.android.diploma.domain.models.Area
 import ru.practicum.android.diploma.ui.filtration.adapters.viewholders.RegionViewHolder
 
 class RegionAdapter(
     private val onClickRegion: (Area) -> Unit,
-) : ListAdapter<Area, RegionViewHolder>(RegionComparator()) {
+) : RecyclerView.Adapter<RegionViewHolder>() {
+    val areas = ArrayList<Area>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RegionViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return RegionViewHolder(
@@ -18,21 +19,13 @@ class RegionAdapter(
         )
     }
 
+    override fun getItemCount(): Int = areas.size
+
     override fun onBindViewHolder(holder: RegionViewHolder, position: Int) {
-        val item = currentList[position]
+        val item = areas[position]
         holder.bind(item)
         holder.itemView.setOnClickListener {
             onClickRegion(item)
-        }
-    }
-
-    private class RegionComparator : DiffUtil.ItemCallback<Area>() {
-        override fun areItemsTheSame(oldItem: Area, newItem: Area): Boolean {
-            return oldItem.id == newItem.id
-        }
-
-        override fun areContentsTheSame(oldItem: Area, newItem: Area): Boolean {
-            return oldItem.id == newItem.id
         }
     }
 
