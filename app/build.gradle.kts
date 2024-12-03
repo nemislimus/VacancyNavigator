e2cc18b6215325a5a83b37b30a49fade97786b79
Andrey <amdoit@yandex.ru>
2024-12-03 15:56:11 +0700

    #269 тест сборки минифицированной версии приложения на Гитхабе

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,11 @@ android {
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+        debug {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
